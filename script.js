105ad271fd9cf4da115973fe8a2225adc39ada25
Kshitij Kumar <kshitij27kumar@gmail.com>
2023-03-06 21:18:45 +0530

    Seek video functionality

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const timeElapsed = document.getElementById('time-elapsed')
 const duration = document.getElementById('duration')
 const progressBar = document.getElementById('progress-bar')
 const seek = document.getElementById('seek')
+const seekTooltip = document.getElementById('seek-tooltip')
 
 const videoWorks = document.createElement('video').canPlayType
 if (videoWorks) {
@@ -42,18 +43,42 @@ initializeVideo = () => {
 }
 
 updateTimeElapsed = () => {
-  const time = formatTime(video.currentTime)
+  const time = formatTime(Math.round(video.currentTime))
   timeElapsed.innerText = `${time.minutes}:${time.seconds}`
   timeElapsed.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`)
 }
 
-updateProgressBar = () => {
-  seek.value = Math.floor(video.currentTime)
-  progressBar.value = Math.floor(video.currentTime)
+updateProgress = () => {
+  seek.value = Math.round(video.currentTime)
+  progressBar.value = Math.round(video.currentTime)
 }
+
+updateSeekTooltip = (event) => {
+  const skipTo = Math.round(
+    (event.offsetX / event.target.clientWidth) *
+      parseInt(event.target.getAttribute('max'), 10)
+  )
+  seek.setAttribute('data-seek', skipTo)
+  const t = formatTime(skipTo)
+  seekTooltip.textContent = `${t.minutes}:${t.seconds}`
+  const rect = video.getBoundingClientRect()
+  seekTooltip.style.left = `${event.pageX - rect.left}px`
+}
+
+skipAhead = (event) => {
+  const skipTo = event.target.dataset.seek
+    ? event.target.dataset.seek
+    : event.target.value
+  video.currentTime = skipTo
+  progressBar.value = skipTo
+  seek.value = skipTo
+}
+
 playButton.addEventListener('click', togglePlay)
 video.addEventListener('play', updatePlayButton)
 video.addEventListener('pause', updatePlayButton)
 video.addEventListener('loadedmetadata', initializeVideo)
 video.addEventListener('timeupdate', updateTimeElapsed)
-video.addEventListener('timeupdate', updateProgressBar)
+video.addEventListener('timeupdate', updateProgress)
+seek.addEventListener('mousemove', updateSeekTooltip)
+seek.addEventListener('input', skipAhead)
